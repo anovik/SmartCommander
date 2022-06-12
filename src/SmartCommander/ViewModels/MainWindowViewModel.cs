71475cb57faebb5e7596fe328598270d959cceae
Anna Novikova <anna@elangroupsoftware.com>
2022-06-12 14:19:10 +0300

    Added basic menu.

--- src/SmartCommander/ViewModels/MainWindowViewModel.cs
+++ src/SmartCommander/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,26 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using ReactiveUI;
+using System.Reactive;
+
 namespace SmartCommander.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {    
         public MainWindowViewModel()
-        {           
-            
-        }       
+        {
+            ExitCommand = ReactiveCommand.Create(Exit);
+        }      
+
+        public ReactiveCommand<Unit, Unit> ExitCommand { get; }
+
+
+        public void Exit()
+        {
+            if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+            {                
+                desktopLifetime.Shutdown();
+            }
+        }
     }
 }
