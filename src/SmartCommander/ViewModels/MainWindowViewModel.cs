dd8206bd5047e6075868299fbdd6795976689fe5
Anna Novikova <anna@elangroupsoftware.com>
2022-08-16 15:42:35 +0300

    Copy and move code.

--- src/SmartCommander/ViewModels/MainWindowViewModel.cs
+++ src/SmartCommander/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using MessageBox.Avalonia.Enums;
 using ReactiveUI;
 using SmartCommander.Models;
 using System;
+using System.IO;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
@@ -189,8 +190,15 @@ namespace SmartCommander.ViewModels
             var copy = new CopyMoveViewModel(true, SelectedPane.CurrentItem, SecondPane.CurrentDirectory);            
             var result = await ShowCopyDialog.Handle(copy);
             if (result != null)
-            {
-
+            {            
+                if (SelectedPane.CurrentItem.IsFolder)
+                {
+                    // copy folder
+                }
+                else
+                {
+                    // copy file
+                }
             }
         }
 
@@ -200,7 +208,14 @@ namespace SmartCommander.ViewModels
             var result = await ShowCopyDialog.Handle(copy);
             if (result != null)
             {
-
+                if (SelectedPane.CurrentItem.IsFolder)
+                {
+                    // move folder
+                }
+                else
+                {
+                    // move file
+                }
             }
         }
 
@@ -240,6 +255,39 @@ namespace SmartCommander.ViewModels
             {                
                 SelectedPane.Delete();
             }
-        }       
+        }
+
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
     }
 }
