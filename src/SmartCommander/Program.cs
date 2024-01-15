6918c3539750cdd5e64844d6c5c45a2d794363ea
Anna Novikova <demonplus@gmail.com>
2024-01-15 15:25:43 +0100

    Added initial logic for single instance.

--- src/SmartCommander/Program.cs
+++ src/SmartCommander/Program.cs
@@ -1,17 +1,34 @@
 using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
+using System.IO;
 
 namespace SmartCommander
 {
     internal class Program
     {
+        static FileStream _lockFile;
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            var dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SmartCommander");
+            Directory.CreateDirectory(dir);
+            try
+            {
+                _lockFile = File.Open(Path.Combine(dir, ".lock"), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                _lockFile.Lock(0, 0);
+                BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+            }
+            catch
+            {
+               // TODO: need to activate another process
+            }
+           
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
